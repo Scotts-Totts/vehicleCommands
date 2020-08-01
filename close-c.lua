55c31c44384db4b6c7cd4a8faab90847c0a3e73a
Scotts-Totts <69021545+Scotts-Totts@users.noreply.github.com>
2020-08-01 11:35:50 -0400

    Update close-c.lua
    added comments

--- close-c.lua
+++ close-c.lua
@@ -1,4 +1,4 @@
-RegisterCommand("close", function(source,args)
+RegisterCommand("close", function(source,args)--  close door, usage /close (1-4)
     closeDoor(tonumber(args[1])-1)
     notify("Door closed.")
 end, false)
