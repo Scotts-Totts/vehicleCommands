71375d1f9869273f7ad4e2c1087c2e754e182846
Scotts-Totts <69021545+Scotts-Totts@users.noreply.github.com>
2020-08-01 13:53:40 -0400

    Update wash-c.lua
    fixed shit

--- wash-c.lua
+++ wash-c.lua
@@ -1,4 +1,4 @@
-RegisterCommand("wash", function(source)-- clean dirt off vehicle usage /wash
+RegisterCommand("wash", function(source) -- clean dirt off vehicle usage /wash
     if isTargetDriving() then
         SetVehicleDirtLevel(pedVehicle, 0)
         notify("~g~Vehicle has been cleaned!")
