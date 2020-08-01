026d3c0d257f74b1fe80daedf29cae7343ed2738
Scotts-Totts <69021545+Scotts-Totts@users.noreply.github.com>
2020-08-01 11:36:46 -0400

    Update open-c.lua
    added comments

--- open-c.lua
+++ open-c.lua
@@ -1,4 +1,4 @@
-RegisterCommand("open", function(source,args)
+RegisterCommand("open", function(source,args)--  open door, usage /open (1-5)
     openSesame(tonumber(args[1])-1)
     notify("Door opened.")
 end, false)
