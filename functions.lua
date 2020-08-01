d97232daa4855b1de263f0fe3cfb1239b3b5bd79
Scotts-Totts <69021545+Scotts-Totts@users.noreply.github.com>
2020-08-01 13:52:47 -0400

    Update functions.lua
    fixed shit not working

--- functions.lua
+++ functions.lua
@@ -1,4 +1,4 @@
-ped = GetPlayerPed(GetPlayerFromServerId(source))
+ped = GetPlayerPed(-1)
 pedVehicle = GetVehiclePedIsIn(ped, false) -- returns 0 if ped is not in vehicle
 
 function isTargetDriving() -- checks if global var ped is driving the vehicle 
@@ -18,15 +18,48 @@ function openSesame(door) -- function to open door
         SetVehicleDoorOpen(pedVehicle, door, false, false) 
         return 
     else
-        notify("You must be the driver of a vehicle to toggle doors")
+        notify("~r~You must be the driver of a vehicle to toggle doors")
     end
 end
+
+function openAll() -- opem all doors
+    if isTargetDriving() then 
+        openSesame(0)
+        openSesame(1)
+        openSesame(2)
+        openSesame(3)
+        openSesame(4)
+        openSesame(5)
+        openSesame(6)
+        openSesame(7)
+        return
+    else
+        notify("~r~You must be the driver of a vehicle to toggle doors") 
+    end
+end
+
 function closeDoor(door) -- function to close door
     if isTargetDriving() then 
         SetVehicleDoorShut(pedVehicle, door, false) 
         return
     else
-        notify("You must be the driver of a vehicle to toggle doors") 
+        notify("~r~You must be the driver of a vehicle to toggle doors") 
+    end
+end
+
+function closeAll() -- close all doors
+    if isTargetDriving() then 
+        closeDoor(0)
+        closeDoor(1)
+        closeDoor(2)
+        closeDoor(3)
+        closeDoor(4)
+        closeDoor(5)
+        closeDoor(6)
+        closeDoor(7)
+        return
+    else
+        notify("~r~You must be the driver of a vehicle to toggle doors") 
     end
 end
 
