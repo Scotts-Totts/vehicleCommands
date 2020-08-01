05a8dbf9c1e4b3bf05f7e2679d6f533407e59723
Scotts-Totts <69021545+Scotts-Totts@users.noreply.github.com>
2020-08-01 13:53:14 -0400

    Update open-c.lua
    FIXED SHIT

--- open-c.lua
+++ open-c.lua
@@ -1,4 +1,9 @@
-RegisterCommand("open", function(source,args)--  open door, usage /open (1-5)
-    openSesame(tonumber(args[1])-1)
-    notify("Door opened.")
+RegisterCommand("open", function(source,args) --  open door, usage /open (1-5)
+    if args[1] == nil then 
+        openAll()
+        notify("~g~Doors opened.")
+    else
+        openSesame(tonumber(args[1])-1)
+        notify("~g~Door opened.")
+    end
 end, false)
