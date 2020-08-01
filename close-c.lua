78c6df2d0e1499d28ebfae7ab3193b9ccf899627
Scotts-Totts <69021545+Scotts-Totts@users.noreply.github.com>
2020-08-01 13:52:25 -0400

    Update close-c.lua
    fixed shit not working

--- close-c.lua
+++ close-c.lua
@@ -1,4 +1,9 @@
-RegisterCommand("close", function(source,args)--  close door, usage /close (1-4)
-    closeDoor(tonumber(args[1])-1)
-    notify("Door closed.")
+RegisterCommand("close", function(source,args) --  close door, usage /close (1-4)
+    if args[1] == nil then 
+        closeAll()
+        notify("~g~Doors closed.")
+    else
+        closeDoor(tonumber(args[1])-1)
+        notify("~g~Door closed.")
+    end
 end, false)
