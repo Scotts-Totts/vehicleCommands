25aeb480a42287cfede4a76c7976c4ad7c2c1c63
Scotts-Totts <69021545+Scotts-Totts@users.noreply.github.com>
2020-08-02 15:56:24 -0400

    Update door-c.lua

--- door-c.lua
+++ door-c.lua
@@ -1,6 +1,6 @@
 RegisterCommand("door", function(source, args)
     if args[1] == nil then 
-        notify("~r~Incorrect syntax. Usage /door (1-5)")
+        notify("~r~You must be the driver of the vehicle to use this command. Usage /door (1-5)")
     else
         toggleDoor(tonumber(args[1])-1)
         notify("~g~Door toggled.")
